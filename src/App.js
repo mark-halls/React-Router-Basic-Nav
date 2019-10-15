48802d1a0eeaac9d27ebc8d427a7392fd5aa279c
Mark Halls <mark.halls@gmail.com>
2019-10-15 15:26:30 -0700

    Add components to app routes

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ import { Home, About, Contact, Navigation } from "./components";
 
 const App = () => (
   <div>
-    <Route exact path="/" />
-    <Route path="/about" />
-    <Route path="/contact" />
+    <Route exact path="/" component={Home} />
+    <Route path="/about" component={About} />
+    <Route path="/contact" component={Contact} />
     <Navigation />
   </div>
 );
